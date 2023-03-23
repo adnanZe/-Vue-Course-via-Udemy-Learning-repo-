f3177749011278120c022091edf5e8b157ae07c9
AdnanZe <adnan.zecheria@yahoo.com>
2023-03-23 16:49:46 +0200

    adding new contact with form

--- vue-cli-01-a-new-vue-project/src/App.vue
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -3,6 +3,7 @@
     <header>
       <h1>My Friends</h1>
     </header>
+    <new-friend @add-new-friend="onAddNewFried"></new-friend>
     <ul>
       <friend-contact
         v-for="friend in friends"
@@ -20,8 +21,9 @@
 
 <script>
 import FriendContact from "./components/FriendContact.vue";
+import NewFriend from "./components/NewFriend.vue";
 export default {
-  components: { FriendContact },
+  components: { FriendContact, NewFriend },
   data() {
     return {
       friends: [
@@ -48,6 +50,18 @@ export default {
         (friend) => friend.id === friendId
       );
       identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
+
+    onAddNewFried(name, phone, email) {
+      const newFriend = {
+        id: new Date().toISOString(),
+        name,
+        phone,
+        email,
+        isFavorite: false,
+      };
+
+      this.friends.push(newFriend);
     },
   },
 };
@@ -86,7 +100,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -118,5 +133,21 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+
+#app form div {
+  margin: 1rem 0;
 }
 </style>
---
+++ vue-cli-01-a-new-vue-project/src/components/NewFriend.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="onAddNewFriend">
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="inputName" />
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="tel" v-model="inputPhoneNumber" />
+    </div>
+    <div>
+      <label>E-mail</label>
+      <input type="email" v-model="inputEmail" />
+    </div>
+    <button>Add New Friend</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["add-new-friend"],
+  data() {
+    return {
+      inputName: "",
+      inputPhoneNumber: "",
+      inputEmail: "",
+    };
+  },
+  methods: {
+    onAddNewFriend() {
+      this.$emit(
+        "add-new-friend",
+        this.inputName,
+        this.inputPhoneNumber,
+        this.inputEmail
+      );
+    },
+  },
+};
+</script>
+
+<style></style>
