ab093375ab95adc20630387cac9a73936a6f138e
AdnanZe <adnan.zecheria@yahoo.com>
2023-03-23 14:40:13 +0200

    working with components

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from "./components/FriendContact.vue";
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: "ion",
+          name: "Ion Test",
+          phone: "0222 222 333",
+          email: "ion@localhost.com",
+        },
+        {
+          id: "costel",
+          name: "costel Test",
+          phone: "0444 444 222",
+          email: "costel@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "ion",
+        name: "Ion Test",
+        phone: "0222 222 333",
+        email: "ion@localhost.com",
+      },
+    };
+  },
+
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
+
+<style></style>
