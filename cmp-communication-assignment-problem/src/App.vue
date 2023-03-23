92ba33f74aa10cdd17b8aad2bc51da1349555366
AdnanZe <adnan.zecheria@yahoo.com>
2023-03-23 17:46:42 +0200

    finished assignment props & custom events

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <user-data @add-active-user="onAddActiveUser"></user-data>
+  <active-user
+    v-for="user in activeUsers"
+    :key="user.name"
+    :username="user.name"
+    :age="user.age"
+  ></active-user>
+</template>
+
+<script>
+import ActiveUser from "./components/ActiveUser.vue";
+import UserData from "./components/UserData.vue";
+export default {
+  components: { UserData, ActiveUser },
+  data() {
+    return {
+      activeUsers: [],
+    };
+  },
+
+  methods: {
+    onAddActiveUser(newActiveUser) {
+      newActiveUser.age = String(newActiveUser.age);
+      this.activeUsers.push(newActiveUser);
+    },
+  },
+};
+</script>
+
+<style>
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+form div {
+  margin: 2rem;
+}
+
+form label {
+  margin-right: 1rem;
+}
+
+form label::after {
+  content: ":";
+}
+</style>
---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="inputName" />
+    </div>
+    <div>
+      <label>Age</label>
+      <input type="number" v-model="inputAge" />
+    </div>
+    <button>Add Active User</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["add-active-user"],
+  data() {
+    return {
+      inputName: "",
+      inputAge: "",
+    };
+  },
+  methods: {
+    submitData() {
+      const newActiveUser = {
+        name: this.inputName,
+        age: this.inputAge,
+      };
+
+      this.$emit("add-active-user", newActiveUser);
+    },
+  },
+};
+</script>
+
+<style></style>
