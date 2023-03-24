c00d9d813f63378c7c4a251c4173f562d03ef5ae
AdnanZe <adnan.zecheria@yahoo.com>
2023-03-24 11:07:23 +0200

    wip

--- prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
+++ prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
@@ -16,7 +16,7 @@ export default {
   components: {
     LearningResources,
   },
-  props: {
+  inject: {
     resources: {
       type: Object,
       required: true,
---
+++ prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,54 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')"
+      >Stored Resources</base-button
+    >
+    <base-button @click="setSelectedTab('add-resource')"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+import AddResource from './AddResource.vue';
+import StoredResources from './StoredResources.vue';
+
+export default {
+  components: {
+    StoredResources,
+    AddResource,
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google ...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+};
+</script>
+
+<style></style>
