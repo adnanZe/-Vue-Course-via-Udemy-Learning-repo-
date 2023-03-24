62386951ed9ead9c1e6df35727dca534cac2999e
AdnanZe <adnan.zecheria@yahoo.com>
2023-03-24 09:56:08 +0200

    project setup for Learning resource app

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <li v-for="resource in storedResources" :key="resource.id">
+      <h3>{{ resource.title }}</h3>
+      <p>{{ resource.description }}</p>
+      <a :href="resource.link">link</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google ...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
