1168c288606894db4202bec45797ce1e05fa084b
AdnanZe <adnan.zecheria@yahoo.com>
2023-03-26 23:29:32 +0300

    working with vue-router

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -1,7 +1,19 @@
 import { createApp } from 'vue';
-
+import { createRouter, createWebHashHistory } from 'vue-router';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
 import App from './App.vue';
 
-const app = createApp(App)
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    { path: '/', component: TeamsList },
+    { path: '/users', component: UsersList },
+  ],
+});
+
+const app = createApp(App);
+
+app.use(router);
 
 app.mount('#app');
