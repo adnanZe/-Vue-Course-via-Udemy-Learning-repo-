f7ff4ec5bc132cb4fdf3354b9a756bf6ad7e535d
AdnanZe <adnan.zecheria@yahoo.com>
2023-03-27 09:06:55 +0300

    route with params

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createRouter, createWebHashHistory } from 'vue-router';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 import App from './App.vue';
 
 const router = createRouter({
@@ -9,6 +10,7 @@ const router = createRouter({
   routes: [
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
+    { path: '/teams/:teamId', component: TeamMembers },
   ],
   linkActiveClass: 'active',
 });
