eff70b68c8d2f32dcd9ea293869e6f7b09437007
AdnanZe <adnan.zecheria@yahoo.com>
2023-04-02 16:50:20 +0300

    added coach data

--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -5,10 +5,25 @@
       <button>Refresh</button>
       <router-link to="/register">Register Coach</router-link>
     </div>
-    <ul>
-      LIST OF COACHES
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
     </ul>
-
-    LIST OF COACHES
+    <h3 v-else>No coaches found.</h3>
   </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
+</script>
