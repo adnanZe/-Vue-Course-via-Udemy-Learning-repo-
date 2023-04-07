146acf663753546684d9724c022074cbb1633e3a
AdnanZe <adnan.zecheria@yahoo.com>
2023-04-07 17:47:21 +0300

    wip requests page

--- main-prj-01-starting-setup/src/pages/requests/RequestsReceived.vue
+++ main-prj-01-starting-setup/src/pages/requests/RequestsReceived.vue
@@ -1,1 +1,36 @@
-<template>REQUESTS</template>
+<template>
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests Received</h2>
+      </header>
+      <ul></ul>
+      <h3>You haven't received any requests yet!</h3>
+    </base-card>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    receivedRequests() {},
+  },
+};
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
