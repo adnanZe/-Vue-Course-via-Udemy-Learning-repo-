16cd4d4e04a4bfd27e6cd196e426b4a1fe8583fc
AdnanZe <adnan.zecheria@yahoo.com>
2023-04-08 11:34:06 +0300

    connect project with firebase and added PUT method to add coaches

--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -5,7 +5,7 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
+        <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
         <base-button v-if="!isCoach" link to="/register"
           >Register Coach</base-button
         >
@@ -75,6 +75,13 @@ export default {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
     },
+
+    loadCoaches() {
+      this.$store.dispatch('coaches/loadCoaches');
+    },
+  },
+  created() {
+    this.loadCoaches();
   },
 };
 </script>
