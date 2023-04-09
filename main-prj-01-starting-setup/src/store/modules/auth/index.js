5bbf2b920981177395556f56112033db4d5b81d4
AdnanZe <adnan.zecheria@yahoo.com>
2023-04-09 13:55:01 +0300

    prepared auth file/folder structure to manage in vuex

---
+++ main-prj-01-starting-setup/src/store/modules/auth/index.js
@@ -0,0 +1,15 @@
+import getters from './getters';
+import actions from './actions';
+import mutations from './mutations';
+
+export default {
+  state() {
+    return {
+      userId: 'c3',
+    };
+  },
+
+  getters,
+  actions,
+  mutations,
+};
