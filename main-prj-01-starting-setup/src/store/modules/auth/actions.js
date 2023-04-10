9a933d45fa2ae69b823030b927d69d9b76dc21e8
AdnanZe <adnan.zecheria@yahoo.com>
2023-04-10 19:44:04 +0300

    refactor and add itToken and localId to localStorage

--- main-prj-01-starting-setup/src/store/modules/auth/actions.js
+++ main-prj-01-starting-setup/src/store/modules/auth/actions.js
@@ -1,44 +1,36 @@
 export default {
   async login(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAOHo4DDRJKSNcHYZwJTUlNdQcvJ5-5Sio',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      const error = new Error(
-        responseData.message || 'Failed to authenticate. Check your login data.'
-      );
-      throw error;
-    }
-
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
     });
   },
   async signup(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAOHo4DDRJKSNcHYZwJTUlNdQcvJ5-5Sio',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
 
+  async auth(context, payload) {
+    const mode = payload.mode;
+
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAOHo4DDRJKSNcHYZwJTUlNdQcvJ5-5Sio';
+
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAOHo4DDRJKSNcHYZwJTUlNdQcvJ5-5Sio';
+    }
+
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
     const responseData = await response.json();
 
     if (!response.ok) {
@@ -47,6 +39,9 @@ export default {
       );
       throw error;
     }
+
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('token', responseData.localId);
 
     context.commit('setUser', {
       token: responseData.idToken,
