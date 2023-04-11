b436eb137be2e0e3bf829e4d3d740216c5964bb3
AdnanZe <adnan.zecheria@yahoo.com>
2023-04-11 20:11:44 +0300

    compositions API vs Options API test

--- composition-assignment-1-problem/src/App.vue
+++ composition-assignment-1-problem/src/App.vue
@@ -2,9 +2,11 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <h3 v-if="isVisible">
+    {{ goal }}
+  </h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button>Toggle Goal</button>
+  <button @click="onToggleGoal">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -13,9 +15,53 @@
 </template>
 
 <script>
+// import
+//  ref,
+// reactive,
+// toRefs
+// 'vue';
+
 export default {
-  
-}
+  // setup() {
+  //   // const myGoal = ref('Learning Vue');
+  //   // const isVisible = ref(true);
+  //   // const goal = ref({
+  //   //   title: 'Learning Vue',
+  //   //   isVisible: true,
+  //   // });
+  //   const goal = reactive({
+  //     title: 'Learning Vue',
+  //     isVisible: true,
+  //   });
+
+  //   const onToggleGoal = () => {
+  //     // isVisible.value = !isVisible.value;
+  //     // goal.value.isVisible = !goal.value.isVisible;
+  //     goal.isVisible = !goal.isVisible;
+  //   };
+
+  //   return {
+  //     // goal: myGoal.value,
+  //     // isVisible,
+  //     // goal,
+  //     goal,
+  //     onToggleGoal,
+  //   };
+  // },
+
+  data() {
+    return {
+      goal: 'Learning Vue',
+      isVisible: true,
+    };
+  },
+
+  methods: {
+    onToggleGoal() {
+      this.isVisible = !this.isVisible;
+    },
+  },
+};
 </script>
 
 <style>
